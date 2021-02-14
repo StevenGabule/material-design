c0e96981170945c83eba708f52f9d19fe53e49af
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-02-14 23:45:03 +0800

    learn the material the basic to advanced

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  makeStyles,
+  Paper,
+  Radio,
+  RadioGroup,
+} from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+    backgroundColor: "#424242",
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
 function App() {
+  const [spacing, setSpacing] = useState(2);
+  const classes = useStyles();
+
+  // change the space of columns
+  const handleChange = (e) => {
+    setSpacing(Number(e.target.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container
+        maxWidth={"sm"}
+        style={{ backgroundColor: "#eee", padding: "5%", marginBottom: "5%" }}
+        fixed
+      >
+        <Button variant={"contained"} color={"primary"}>
+          Hello World
+        </Button>
+        <Box color={"teal"} component={"p"} m={1}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet
+          aspernatur debitis, distinctio dolor dolores doloribus eum impedit, in
+          maiores nisi obcaecati perferendis provident, quo ut vero voluptas!
+          Asperiores, quas.
+        </Box>
+      </Container>
+
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={spacing}>
+            {[0, 1, 2].map((value) => (
+              <Grid key={value} item>
+                <Paper className={classes.paper} square={true} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>{" "}
+        {/*Item Grid */}
+        <Grid item xs={12}>
+          <Paper className={classes.control}>
+            <Grid container justify="center">
+              <Grid item>
+                <FormLabel>Spacing</FormLabel>
+                <RadioGroup
+                  name="spacing"
+                  aria-label="spacing"
+                  value={spacing.toString()}
+                  onChange={handleChange}
+                  row
+                >
+                  {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
+                    <FormControlLabel
+                      key={value}
+                      value={value.toString()}
+                      control={<Radio />}
+                      label={value.toString()}
+                    />
+                  ))}
+                </RadioGroup>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
